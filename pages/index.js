784c6afa0fc7ce90c07301547954a8b5178cc0a8
Tanmay Pathak <tanmay.pathak@usask.ca>
2022-03-14 19:50:19 -0600

    added auto theme and fixed formatting

--- pages/index.js
+++ pages/index.js
@@ -38,11 +38,11 @@ export default function Home() {
             {/* 
           ## Getting Started with LittleLink
           This page has been built with every pre-designed button available in LittleLink by default. You can rearrange and delete as needed.
-          You can add your own brand or others brands you may need in the `css/brands.css` file. 
-          You can add custom icons to `/icons/...`. It is recommended to use a 24x24 .SVG.
+          You can add your own brand or others brands you may need in the `styles/brands.css` file. 
+          You can add custom icons to `/public/icons/...`. It is recommended to use a 24x24 .SVG.
           Edit the "Your Image Here" section to add your own personal branding, like a picture of yourself or your brand logo!
           Edit the "Title" section to change the page heading. You can use something like your name, your social handle, or your brand name.
-          Edit the "Short Bio" section tell users about yourself or your brand.
+          Edit the "Short Intro" section tell users about yourself or your brand.
            */}
 
             {/* Your Image Here */}
